6ddfc6d18c71c35cacd717c38efe74c2cd02aac0
Lokesh Metta <26522232+lokeshmetta@users.noreply.github.com>
2023-12-07 16:14:26 +0530

    dsf

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ import { themes as prismThemes } from "prism-react-renderer";
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "Bhukamatha",
-  tagline: "A Tool to Discover possible",
+  tagline: "A Tool to Discover",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
