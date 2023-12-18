30ffc9032e59ed22ecfafae743f074bf845f483d
Lokesh Metta <26522232+lokeshmetta@users.noreply.github.com>
2023-12-18 22:18:36 +0530

    update

--- src/components/DownloadButton.js
+++ src/components/DownloadButton.js
@@ -5,8 +5,8 @@ const DownloadButton = () => {
     <button className="download-button" role="button">
       Download
       <div className="dropdown-content">
-        <a href="/docs/">Latest Release</a>
-        <a href="#">Old Releases</a>
+        <a href="./404">Latest Release</a>
+        <a href="./404">Old Releases</a>
         {/* Add more versions as needed */}
       </div>
     </button>
