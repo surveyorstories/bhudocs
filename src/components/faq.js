1098cf677e35a69f83d1f0197fd1ad0cab9c9284
Lokesh Metta <26522232+lokeshmetta@users.noreply.github.com>
2023-12-18 14:02:33 +0530

    ff

--- src/components/faq.js
+++ src/components/faq.js
@@ -13,33 +13,57 @@ const Accordion = () => {
 
   const accordionData = [
     {
-      question: "Question 1",
-      answer: "Answer 1 goes here...",
+      question:
+        "Does Bhukamatha have an officially developed plugin? Should I pay anything for using Bhukamatha?",
+      answer:
+        "No, Bhukamatha is developed by the SurveyorStories community and backed by Village surveyors; you don't need to pay anything for using it.",
     },
     {
-      question: "Question 2",
-      answer: "Answer 2 goes here...",
+      question: "Can I use Bhukamatha to generate LPMS?",
+      answer:
+        "Yes, it also supports the Stone map from version 1.0.1 beta. It's up to you whether to use it or not.",
+    },
+    {
+      question: "How would Bhukamatha prevent the misuse of data?",
+      answer:
+        "Bhukamatha doesn't hold or provide any data. The complete data you processed lies on your PC. Hence, no one can make the LPMs unless they have the data.",
+    },
+    {
+      question:
+        "Is Team Bhukamatha was interested in Collaborate/ Contribution? ",
+      answer:
+        "We are happy to collaborate with you for building a better plugin. You can make a difference at Bhukamatha by contributing to new features, documentation, and portal updates.. you can reach us. at surveyorstories@gmail.com",
+    },
+    {
+      question: "Does Bhukamatha offer any guarantee of work or service? ",
+      answer:
+        "No, we cannot give you any guarantee of work or support. We are not responsible for any loss of work; We are only helping to fix any bugs identified during the process. It's up to you to take the risk.",
     },
     // Add more questions and answers as needed
   ];
 
   return (
-    <div className="accordion">
-      {accordionData.map((item, index) => (
-        <div className="accordion-item" key={index}>
-          <div
-            className="accordion-header"
-            onClick={() => handleAccordionClick(index)}
-          >
-            <h3 className="accordion-title">{item.question}</h3>
-          </div>
-          {activeIndex === index && (
-            <div className="accordion-content">
-              <p>{item.answer}</p>
+    <div className="accordion-container">
+      <h1 className="faq-title">FAQ</h1>
+      <div className="accordion">
+        {accordionData.map((item, index) => (
+          <div className="accordion-item" key={index}>
+            <div
+              className="accordion-header"
+              onClick={() => handleAccordionClick(index)}
+            >
+              <h3 className="accordion-title">{item.question}</h3>
             </div>
-          )}
-        </div>
-      ))}
+            <div
+              className={`accordion-content ${
+                activeIndex === index ? "open" : ""
+              }`}
+            >
+              <p className="accordion-text">{item.answer}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
