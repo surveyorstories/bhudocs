5f0f8311f73b075a33d312987a28e526a832ead2
Lokesh Metta <26522232+lokeshmetta@users.noreply.github.com>
2023-12-21 21:27:32 +0530

    edit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -87,7 +87,7 @@ const config = {
           sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/surveyorstories/bhudocs",
+          editUrl: "https://github.com/surveyorstories/bhudocs/blob",
         },
         blog: {
           showReadingTime: true,
