87e0ee64173ad526183b6cbca8df26757f304d7d
Lokesh Metta <26522232+lokeshmetta@users.noreply.github.com>
2024-11-19 23:10:39 +0530

    asd

--- blog/first_post.md
+++ blog/first_post.md
@@ -1,3 +1,9 @@
-# Blog
+---
+title: Markdown blog truncation example
+---
 
-## Nothing to Say
+All these will be part of the blog post summary.
+
+<!-- truncate -->
+
+But anything from here on down will not be.
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,29 +49,30 @@ const config = {
     ],
 
     [
-      "@docusaurus/plugin-pwa",
+    
+      '@docusaurus/plugin-pwa',
       {
         debug: true,
         offlineModeActivationStrategies: [
-          "appInstalled",
-          "standalone",
-          "queryString",
+          'appInstalled',
+          'standalone',
+          'queryString',
         ],
         pwaHead: [
           {
-            tagName: "link",
-            rel: "icon",
-            href: "img/192.png",
+            tagName: 'link',
+            rel: 'icon',
+            href: '/img/docusaurus.png',
           },
           {
-            tagName: "link",
-            rel: "manifest",
-            href: "/manifest.json", // your PWA manifest
+            tagName: 'link',
+            rel: 'manifest',
+            href: '/manifest.json', // your PWA manifest
           },
           {
-            tagName: "meta",
-            name: "theme-color",
-            content: "rgb(37, 194, 160)",
+            tagName: 'meta',
+            name: 'theme-color',
+            content: 'rgb(37, 194, 160)',
           },
         ],
       },
