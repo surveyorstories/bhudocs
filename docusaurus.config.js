1762a58817b5dde22d01d0f1542dfaca5853a7f3
Lokesh Metta <26522232+lokeshmetta@users.noreply.github.com>
2024-11-19 23:40:12 +0530

    fg

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,7 +72,7 @@ const config = {
           {
             tagName: 'meta',
             name: 'theme-color',
-            content: 'rgb(37, 194, 162)',
+            content: 'rgb(37, 194, 160)',
           },
         ],
       },
